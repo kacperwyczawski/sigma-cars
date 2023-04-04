098e3fb783615eb4a68d6f1f7f5c57f21f7fc4cf
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-04-04 20:09:44 +0200

    feat(backend): replace minimal api with controller based one

--- Backend/SigmaCars.WebApi/Program.cs
+++ Backend/SigmaCars.WebApi/Program.cs
@@ -1,16 +1,18 @@
 using System.Data;
 using Npgsql;
 using SigmaCars.Application;
-using SigmaCars.Domain;
 using SigmaCars.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddTransient<IDbConnection>(_ =>
     new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ICarModelsDataService, CarModelsDataService>();
+
 
 var app = builder.Build();
 
@@ -23,12 +25,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthorization();
 
-app.MapGet("/car-models", async (ICarModelsDataService carModelsDataService) => 
-    await carModelsDataService.GetAllAsync());
-
-app.MapPost("/car-models", async (ICarModelsDataService carModelsDataService, CarModel carModel) =>
-        await carModelsDataService.AddAsync(carModel));
+app.MapControllers();
 
 app.Run();
