d89e46282b79582dac6d1226ca29c146c1761413
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-04-18 19:34:24 +0200

    feat(frontend): set up frontend placeholder

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Fira_Code} from "next/font/google";
+import {Inter} from 'next/font/google'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const firaCode = Fira_Code({
+    subsets: ['latin'],
+    variable: '--font-fira-code'
+})
+
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter'
+})
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <div className={`${inter.variable} font-sans ${firaCode.variable} font-mono`}>
+            <Component {...pageProps} />
+        </div>
+    )
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,18 +1,24 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './app/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        fontFamily: {
+            sans: ['var(--font-inter)', ...fontFamily.sans],
+            mono: ['var(--font-fira-code)', ...fontFamily.mono],
+        },
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
