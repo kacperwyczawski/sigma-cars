c2caceeb445a7b31ca349300e129b3f2fd7ddcca
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-05-13 14:30:29 +0200

    feat(frontend): add log in and sign up pages

--- Frontend/components/TheHeader.vue
+++ Frontend/components/TheHeader.vue
@@ -1,5 +1,5 @@
 ﻿<template>
-    <header class="flex bg-orange-600 p-2 sm:p-4 justify-between items-center text-white">
+    <header class="flex bg-orange-600 p-2 sm:p-4 justify-between items-center text-white h-16">
         <div class="flex gap-2 text-xl sm:text-2xl font-bold">
             <NuxtLink class="w-8 h-8"
                       href="/">
@@ -11,10 +11,10 @@
             </NuxtLink>
         </div>
         <div class="flex-row font-normal text-lg sm:text-xl divide-x-2">
-            <NuxtLink href="/" class="px-2">
+            <NuxtLink href="/login" class="px-2">
                 Log in
             </NuxtLink>
-            <NuxtLink href="/" class="px-2">
+            <NuxtLink href="/signup" class="px-2">
                 Sign up
             </NuxtLink>
         </div>
---
+++ Frontend/pages/signup.vue
@@ -0,0 +1,62 @@
+<script setup>
+</script>
+<template>
+    <div class="min-h-[calc(100vh-4rem)] grid place-items-center bg-gray-100 p-2">
+        <div class="max-w-md w-full divide-y bg-white border rounded-md">
+            <div class="p-4">
+                <h1 class="text-2xl text-center">Sign up</h1>
+            </div>
+            <form class="p-4">
+                <div class="mb-4">
+                    <label class="block text-gray-800 font-bold mb-2" for="firstName">
+                        First name
+                    </label>
+                    <input
+                        class="border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                        id="firstName"
+                        type="email"
+                        placeholder="First name"
+                        required
+                    >
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-800 font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <input
+                            class="border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                            id="email"
+                            type="email"
+                            placeholder="Email address"
+                            required
+                    >
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-800 font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input
+                            class="border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                            id="password"
+                            type="password"
+                            placeholder="Password"
+                            required
+                    >
+                </div>
+                <div class="flex items-center justify-between">
+                    <button class="group px-4 py-2 hover:bg-orange-600 text-center text-white bg-slate-800 transition-colors duration-75 rounded-md">
+                        Sign up
+                        <span class="group-hover:translate-x-2 inline-block transition-transform">
+                        -&gt;
+                    </span>
+                    </button>
+                    <NuxtLink class="text-sm"
+                              to="/login"
+                    >
+                        Already have an account? Log in!
+                    </NuxtLink>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
