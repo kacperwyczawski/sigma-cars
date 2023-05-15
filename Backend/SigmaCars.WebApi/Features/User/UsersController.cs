2d4b779bcdbdcce70522c8c39e4d2a407bcf1f74
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-05-15 19:41:10 +0200

    feat(backend): add users controller

--- Backend/SigmaCars.WebApi/Features/User/UsersController.cs
+++ Backend/SigmaCars.WebApi/Features/User/UsersController.cs
@@ -1,10 +1,34 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SigmaCars.WebApi.Features.User;
 
 [ApiController]
-[Route("users")]
+[Route("users/{id:int}")]
 public class UsersController
 {
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
     
+    [HttpGet]
+    public async Task<IActionResult> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet("rentals")]
+    public async Task<IActionResult> GetRentals(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpDelete("rentals/{rentalId:int}")]
+    public async Task<IActionResult> DeleteRental(int id, int rentalId)
+    {
+        throw new NotImplementedException();
+    }
 }
