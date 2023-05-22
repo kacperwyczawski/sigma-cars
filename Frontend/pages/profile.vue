ae5924eeef4bb72dd60f98e748c80267fe622df2
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-05-22 13:39:35 +0200

    feat(frontend): add departments to admin profile

--- Frontend/pages/profile.vue
+++ Frontend/pages/profile.vue
@@ -1,10 +1,23 @@
 <script setup>
-import {ShieldCheck} from "lucide-vue-next";
+import {ShieldCheck, Plus} from "lucide-vue-next";
 
 const userData = useUserData();
 const router = useRouter();
 
+if (userData.value === null) {
+    router.push('/login');
+}
+
 const {data} = await useFetch(`/api/users/${userData.value.userId}/rentals`);
+
+let departments = [];
+if (userData.value.role === "admin") {
+    console.log("admin")
+    const {data} = await useFetch(`/api/departments`);
+    departments = data.value === undefined
+        ? []
+        : data.value;
+}
 
 const rentals = data.value.rentals === undefined
     ? []
@@ -69,6 +82,30 @@ function handleLogout() {
                             <span class="text-slate-600">in</span>
                             [Location]
                             <span class="block md:inline md:ml-4"><ButtonCancel>Cancel</ButtonCancel></span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div v-if="userData.role === 'admin'"
+                 class="rounded-md border p-4 mt-4">
+                <div class="flex justify-between">
+                    <h2 class="text-xl">
+                        Departments:
+                    </h2>
+                    <button class="hover:bg-orange-600 text-white bg-slate-800 transition-colors duration-75 rounded-md py-1 px-2 text-sm">
+                        Add
+                    </button>
+                </div>
+                <ul class="mt-4 divide-y">
+                    <li v-for="department in departments"
+                        class="flex py-2 flex-col md:flex-row md:justify-between">
+                        <div class="flex items-center gap-2">
+                            <div class="rounded-full border px-1.5 text-slate-500 text-sm">US</div> <!-- TODO -->
+                            <div class="font-bold">{{department.city}}</div>
+                            <div class="text-slate-500">amongus street 123</div> <!-- TODO -->
+                        </div>
+                        <div>
+                            <ButtonCancel>Remove</ButtonCancel>
                         </div>
                     </li>
                 </ul>
