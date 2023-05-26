f4cf3e1c6f2675884186873a2c183d95e4abd85f
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-05-26 19:16:41 +0200

    refactor(frontend): delete unused code

--- Frontend/components/InputSecondary.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-const model = defineModel();
-</script>
-<template>
-  <input class="border rounded-full focus:outline-none focus:border-orange-600 px-2"
-         v-model="model">
-</template>
